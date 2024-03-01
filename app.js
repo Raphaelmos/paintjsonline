57c2436b58b2410c6c7e2f73e463473674b7117e
Raph <96924364+Raphaelmos@users.noreply.github.com>
2024-03-01 03:52:55 +0100

    Update app.js

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const INITIAL_COLOR = '#2c2c2c';
 
 /* Switched back to old style and will give a look similar to a old Vista Paint MS */
 /* Possibility to add colorPicker similar to this : https://github.com/PitPik/colorPicker */
+/* Fixable issue to have sidepanel by a change in HTML*/
 const canvas = document.querySelector('canvas');
 const mode = document.getElementById('jsMode');
 const ctx = canvas.getContext('2d');
@@ -145,7 +146,7 @@ function changePaintTool(clickedTool) {
 }
 
 // New variable to set eraser size
-let eraserRadius = 10; 
+let eraserRadius = 15; 
 
 // Update eraser size on range change
 function handleRangeChange(event){
